794b58d0a2430beb500c62cfe893f8b880992998
smckissock <scottmckissock@gmail.com>
2018-02-11 08:39:41 -0500

    Add dot net importer

---
+++ Exporter/Exporter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conflicts {
+    
+    class Program {
+
+        static void Main(string[] args) {
+            SpreadsheetGear.IWorkbook workbook = SpreadsheetGear.Factory.GetWorkbook("c:\\trump-conflicts\\Exporter\\Exporter\\data\\trump-conflicts2.xlsx");
+            SpreadsheetGear.IWorksheet worksheet = workbook.Worksheets["trump-conflicts"];
+
+            var h = worksheet.Hyperlinks;
+
+            SpreadsheetGear.IRange cells = worksheet.Cells;
+
+            var loader = new ConflictLoader(cells);
+            Console.ReadLine();
+        }
+    }
+}
