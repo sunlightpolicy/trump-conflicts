53b730bae7755c6910cec94939469eb43b9e526a
smckissock <scottmckissock@gmail.com>
2018-03-28 07:41:40 -0400

    INsert business unit and potential conflict

--- Exporter/Exporter/Phase2Loader.cs
+++ Exporter/Exporter/Phase2Loader.cs
@@ -7,6 +7,45 @@ using System.Globalization;
 
 namespace Phase2 {
 
+    enum Col {
+        Parent3Pct = 1,
+        Parent3,
+        Parent2Pct,
+        Parent2,
+        Parent1Pct,
+        Parent1,
+        PotentialConflictName,
+        PotentialConflictDescription,
+        ConflictingEntity,
+        FamilyMember,
+        FamilyRelationshipDescription,
+        FamilyMemberConflictStatus,
+        Source1,
+        Source1Date,
+        Source2,
+        Source2Date,
+        Source3,
+        Source3Date,
+        Notes,
+        DatePublished
+    };
+
+    //public class Table {
+    //    public string Id;
+    //}
+
+    //public class BusinessUnitTable : Table {
+    //    public string Name;
+    //} 
+
+    public class PotentialConflict {
+        public string Name;
+        public string Description;
+        public string Notes;
+        public DateTime DateChanged;
+    }
+    
+    
     public class Source {
         public string Name;
         public string Link;
@@ -103,8 +142,12 @@ namespace Phase2 {
 
     class Phase2Loader {
 
-        public Phase2Loader(String path) {
+        Dictionary<string, string> BusinessUnits = new Dictionary<string, string>();
+        Dictionary<string, PotentialConflict> PotentialConflicts = new Dictionary<string, PotentialConflict>();
 
+
+        public Phase2Loader(String path) {
+            
             var parentsFile = "Copy of Donald & Melania.html";
 
             //var childrenFile = "Donald Trump Jr., Eric Trump, Ivanka Trump & Jared Kushner.html";
@@ -122,7 +165,14 @@ namespace Phase2 {
             //WriteStoriesCsv(conflicts, "c:\\trump-conflicts\\data\\trump_conflicts_of_interest.csv");
 
             //WriteSql(conflicts, "c:\\trump-conflicts\\Exporter\\Exporter\\Db\\");
-            Console.WriteLine(conflicts.Count.ToString() + " total conflicts");
+            //Console.WriteLine(conflicts.Count.ToString() + " total conflicts");
+
+            WriteSql("c:\\trump-conflicts\\Exporter\\Exporter\\db\\Phase2\\");
+        }
+
+        private void WriteSql(string path) {
+            WriteBusinessUnitScript(path, "4");
+            WritePotentialConflictScript(path, "5");
         }
 
         private void ImportPage(List<Conflict> conflicts, string file) {
@@ -134,20 +184,99 @@ namespace Phase2 {
             var rows = text.Split(new[] { "<tr " }, StringSplitOptions.None);
             foreach (string row in rows) {
                 if (rowNum < max && rowNum > 1) {
-                    AddConflict(conflicts, row, file, rowNum);
+
+                    string theRow = row.Replace("<td></td>", "<td ></td>");
+                    var cols = theRow.Split(new[] { "<td " }, StringSplitOptions.None);
+                    if (cols.Length < 2)
+                        return;
+
+                    AddBusinessUnits(cols);
+                    AddPotentialConflict(cols);
+                    //AddConflict(conflicts, cols, file, rowNum);
                 }
                 rowNum++;
             }
-            Console.WriteLine(conflicts.Count.ToString() + " conflicts");
+            
+            Console.WriteLine(BusinessUnits.Count.ToString() + " BusinesUnits");
+            Console.WriteLine(PotentialConflicts.Count.ToString() + " PotentialConflicts");
+            //Console.WriteLine(conflicts.Count.ToString() + " conflicts");
         }
 
-
-        private void AddConflict(List<Conflict> conflicts, string row, string file, int rowNum) {
-            row = row.Replace("<td></td>", "<td ></td>");
-            var cols = row.Split(new[] { "<td " }, StringSplitOptions.None);
-
-            if (cols.Length < 2)
+        private void AddPotentialConflict(string[] cols) {
+            var name = ConflictingEntity(cols[(int)Col.PotentialConflictName]);
+            if (name == "")
                 return;
+            if (PotentialConflicts.ContainsKey(name))
+                return;
+            
+            var description = Description(cols[(int)Col.PotentialConflictDescription]);
+            var note = Notes(cols[(int)Col.Notes]);
+            var date = DateChanged(cols[(int)Col.DatePublished]);
+            
+            var conflict = new PotentialConflict();
+            conflict.Name = name;
+            conflict.Description = description;
+            conflict.Notes = note;
+            conflict.DateChanged = date;
+
+            PotentialConflicts.Add(name, conflict);
+        }
+
+        private void AddBusinessUnits(string [] cols) {
+            AddBusinessUnit(BusUnit(cols[(int)Col.Parent1]));
+            AddBusinessUnit(BusUnit(cols[(int)Col.Parent2]));
+            AddBusinessUnit(BusUnit(cols[(int)Col.Parent3]));
+
+            AddBusinessUnit(BusUnit(cols[(int)Col.ConflictingEntity]));
+        }
+
+        private void AddBusinessUnit(string unit) {
+            if (unit == "")
+                return;
+            if (!BusinessUnits.ContainsKey(unit))
+                BusinessUnits.Add(unit, BusinessUnits.Count.ToString());
+        }
+
+        private void WriteBusinessUnitScript(string path, string seq) {
+            var strings = new List<string>();
+            strings.Add("USE Trump");
+            strings.Add("GO\r\n");
+
+            foreach (String unit in BusinessUnits.Keys)
+                strings.Add("INSERT INTO BusinessUnit VALUES ('" + unit + "')");
+
+            using (TextWriter tw = new StreamWriter(path + seq + " INSERT BusinessUnit.sql")) {
+                foreach (String s in strings)
+                    tw.WriteLine(s);
+            }
+         }
+
+        private void WritePotentialConflictScript(string path, string seq) {
+            var strings = new List<string>();
+            strings.Add("USE Trump");
+            strings.Add("GO\r\n");
+
+            foreach (PotentialConflict conflict in PotentialConflicts.Values)
+                strings.Add("INSERT INTO PotentialConflict VALUES ('" +
+                    conflict.Name + "', '" +
+                    conflict.Description + "', '" +
+                    conflict.Notes + "', '" +
+                    conflict.DateChanged.ToString() + "', " +
+                    "GetDate(), 1)"
+                );
+
+            using (TextWriter tw = new StreamWriter(path + seq + " INSERT PotentialConflict.sql")) {
+                foreach (String s in strings)
+                    tw.WriteLine(s);
+            }
+        }
+
+        private void AddConflict(List<Conflict> conflicts, string[] cols, string file, int rowNum) {
+            //row = row.Replace("<td></td>", "<td ></td>");
+            //var cols = row.Split(new[] { "<td " }, StringSplitOptions.None);
+
+            //if (cols.Length < 2)
+            //    return;
             //if (cols[1].Contains("></td>"))
             //    return;
             //if (cols[1].Contains("Description</td"))
@@ -193,15 +322,18 @@ namespace Phase2 {
         }
 
         private DateTime GetDate(string dte) {
-
             dte = dte
                 .Replace("Sept.", "September");
-
             DateTime date = Convert.ToDateTime(dte.Replace(".", ""));
-
             return date;
         }
 
+
+        private string BusUnit(string txt) {
+            var subs = txt.Split('>');
+            return
+                subs[1].Replace("</td", "").TrimEnd().TrimStart();
+        }
 
         private string Description(string txt) {
             var subs = txt.Split('>');
@@ -257,7 +389,7 @@ namespace Phase2 {
             var dte = subs[1].Replace("</td", "").TrimEnd().TrimStart();
             return
                 Convert.ToDateTime(dte);
-        }
+        }   
     }
 
 
---
+++ Exporter/Exporter/db/Phase2/5 INSERT PotentialConflict.sql
@@ -0,0 +1,14 @@
+USE Trump
+GO
+
+INSERT INTO PotentialConflict VALUES ('4 Shadow Tree Lane', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('40 Wall Street', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('809 North Canon', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('81 Pine Note Holder', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('845 Un', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('Ace Entertainment (Formerly Trump Casinos Or Trump Taj Mahal)', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('All County Building Supply &Amp; Maintenance', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('Aviation Payroll Company', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('B Plaza Realty', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('Bedford Hills', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
+INSERT INTO PotentialConflict VALUES ('Trump Briarcliff Manor Development (F/K/A Briar Hall Development)', '', '', '7/5/2017 12:00:00 AM', GetDate(), 1)
