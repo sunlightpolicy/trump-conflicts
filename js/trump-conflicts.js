bb673533d6d7639a1db03b28a92b831af7322323
smckissock <scottmckissock@gmail.com>
2018-06-20 02:26:44 -0400

    Improve conflict screen

--- js/trump-conflicts.js
+++ js/trump-conflicts.js
@@ -1,5 +1,4 @@
 
-
 var changeDateChart;
 var sourceTypeChart;
 
@@ -107,13 +106,13 @@ d3.json("data/stories.json", function (err, data) {
         .dimension(changeDateDim)
         .group(changeDateGroup)
         //.x(d3.time.scale().domain([new Date(2013, 2, 15), new Date(2018, 3, 31)]))
-        .x(d3.time.scale().domain([new Date(2016, 2, 15), new Date(2018, 3, 31)]))
+        .x(d3.time.scale().domain([new Date(2016, 2, 15), new Date(2018, 6, 15)]))
         .xUnits(d3.time.day)
         .width(leftWidth)
-        .height(140)
-        .margins({ top: 5, right: 30, bottom: 30, left: 50 })
+        .height(240)
+        .margins({ top: 15, right: 20, bottom: 20, left: 40 })
         .elasticY(true)
-        .filter([new Date(2017, 9, 25), new Date(2018, 2, 31)]) // Months are zero based
+        .filter([new Date(2016, 2, 25), new Date(2018, 6, 10)]) // Months are zero based
     changeDateChart.yAxis().ticks(5);
     changeDateChart.xAxis().ticks(5);
     
@@ -144,7 +143,7 @@ d3.json("data/stories.json", function (err, data) {
         .group(function (d) {
             return conflictHeader(d) + ethicsPopupLink(d);
         })  
-        .size(9)  // Remove this and add scroll!!
+        .size(20)  // Remove this and add scroll!!
         .columns([
             function (d) { return dateToYMD(d.sourceDate); },
             function(d) { return d.link; }
@@ -159,14 +158,14 @@ d3.json("data/stories.json", function (err, data) {
 });
 
 function conflictHeader(d) {
-    return "<b>" + d.conflict + "</b> <em>(" + d.familyMember + " / " + d.category + ")</em> " + d.description;
+    return "<b>" + d.conflict + "</b> <em>(" + d.familyMember + " / " + d.category + ")</em> "; // + d.description;
 }
 
 function ethicsPopupLink(d) {
     let link = ""; 
     //if (d.hasEthics)
         //link = " <a href=\"#\" onclick=\"ethicsPopup(" + d.conflictId + "); return false\"><b>Ethics Report</b></a>"
-        link = " <a href=\"#\" onclick=\"timeline(" + d.conflictId + "); return false\"><b>Timeline</b></a>"
+        link = " <a href=\"#\" onclick=\"timelinePopup(" + d.conflictId + "); return false\"><b>Timeline</b></a>"
     return link;
 }
 
@@ -196,101 +195,119 @@ function ethicsPopup(conflictId) {
     });
 }
 
-function timeline(conflictId) {
-    d3.json("data/media/" + conflictId + ".js", function (err, conflictData) {
-        
-        var span = document.getElementsByClassName("close")[0];
-        span.onclick = function () {
+function timelinePopup(conflictId) {
+    d3.json("data/media/" + conflictId + ".js", makeTimelinePopup);
+}
+
+
+function makeTimelinePopup(err, conflictData) {
+    
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal)
             modal.style.display = "none";
-        }
-        window.onclick = function (event) {
-            if (event.target == modal)
-                modal.style.display = "none";
-        }
-        //console.log(data);
+    }
+    //console.log(data);
 
-        let modal = document.getElementById('ethicsModal');
-        modal.style.display = "block";
+    let modal = document.getElementById('ethicsModal');
+    modal.style.display = "block";
 
-        var conflict = d3.select('#conflict');
-        conflict.text(conflictData.name);
+    var conflict = d3.select('#conflict');
+    conflict.text(conflictData.name);
 
-        var conflictDescription = d3.select('#conflictDescription');
-        conflictDescription.text(conflictData.description);
+    var conflictDescription = d3.select('#conflictDescription');
+    conflictDescription.text(conflictData.description);
 
-        //addEthicsDocuments(d3.select('#ethicsModalBody'), data);
-        const ONE_HOUR = 60 * 60 * 1000,
-        ONE_DAY = 24 * ONE_HOUR,
-        ONE_WEEK = 7 * ONE_DAY,
-        ONE_MONTH = 30 * ONE_DAY,
-        SIX_MONTHS = 6 * ONE_MONTH;
-        TWO_YEARS = 24 * ONE_MONTH;
-  
-  var data = [],
+    //addEthicsDocuments(d3.select('#ethicsModalBody'), data);
+    const ONE_HOUR = 60 * 60 * 1000,
+    ONE_DAY = 24 * ONE_HOUR,
+    ONE_WEEK = 7 * ONE_DAY,
+    ONE_MONTH = 30 * ONE_DAY,
+    SIX_MONTHS = 6 * ONE_MONTH;
+    TWO_YEARS = 24 * ONE_MONTH;
+
+    var data = [],
     start = new Date('2016-06-02T20:14:22.691Z'),
     today = new Date('2018-06-08T17:59:06.134Z');
- 
-  var mediaOutlets = conflictData.mediaOutlets
-  for (var x in mediaOutlets) { 
-    data[x] = {};
-    data[x].name = mediaOutlets[x].name;
-    data[x].data = [];
-    for (var y in mediaOutlets[x].data) {
-      data[x].data.push({});
-      data[x].data[y].date = new Date(mediaOutlets[x].data[y].date);
-      data[x].data[y].details = mediaOutlets[x].data[y].details;
+
+    var mediaOutlets = conflictData.mediaOutlets
+    for (var x in mediaOutlets) { 
+        data[x] = {};
+        data[x].name = mediaOutlets[x].name;
+        data[x].data = [];
+        for (var y in mediaOutlets[x].data) {
+            data[x].data.push({});
+            data[x].data[y].date = new Date(mediaOutlets[x].data[y].date);
+            data[x].data[y].details = mediaOutlets[x].data[y].details;
+        }
+        //$('#timeline-selectpicker').append("<option>" + data[x].name + "</option>");
+        data[x].display = true;
     }
-    //$('#timeline-selectpicker').append("<option>" + data[x].name + "</option>");
-    data[x].display = true;
-  }
-  //$('#timeline-selectpicker').selectpicker('selectAll');
-  
-  var timeline = d3.chart.timeline()
-    .end(today)
-    .start(today - TWO_YEARS)
-    .minScale(ONE_WEEK / ONE_MONTH)
-    .maxScale(ONE_WEEK / ONE_HOUR)
-    .slider(false)  // Scale thing to the right
-    .context(false) // Brush below the timeline
-    .marker(false)  // Floating popup under mouse with date range
-    .lineHeight(15)
-    //.eventPopover("HI")
-    .eventClick(function(el) {
-        debugger;
-      var table = '<table class="table table-striped table-bordered">';
-      if(el.hasOwnProperty("events")) {
-        table = table + '<thead>This is a group of ' + el.events.length + ' events starting on '+ el.date + '</thead><tbody>';
-        table = table + '<tr><th>Date</th><th>Event</th><th>Object</th></tr>';
-        for (var i = 0; i < el.events.length; i++) {
-          table = table + '<tr><td>' + el.events[i].date + ' </td> ';
-          for (var j in el.events[i].details) {
-            table = table +'<td> ' + el.events[i].details[j] + ' </td> ';
-          }
-          table = table + '</tr>';
-        }
-        table = table + '</tbody>';
-      } else {
-        table = table + 'Date: ' + el.date + '<br>';
-        for (i in el.details) {
-          table = table + i.charAt(0).toUpperCase() + i.slice(1) + ': ' + el.details[i] + '<br>';
-        }
-      }
-      $('#legend').html(table);
-  
-    });
-    /* if(countNames(data) <= 0) {
-        timeline.labelWidth(60);
-    } */
-  
-  
-    // Keep this
+    //$('#timeline-selectpicker').selectpicker('selectAll');
+
+    var timeline = d3.chart.timeline()
+        .end(today)
+        .start(today - TWO_YEARS)
+        .minScale(ONE_WEEK / ONE_MONTH)
+        .maxScale(ONE_WEEK / ONE_HOUR)
+        .slider(false)  // Scale thing to the right
+        .context(false) // Brush below the timeline
+        .marker(false)  // Floating popup under mouse with date range
+        .lineHeight(15)
+        .eventClick(popup());
+
+    addStories(mediaOutlets);
+
     d3.select('#timeline').selectAll("svg").remove();
     var element = d3.select('#timeline').append('div').datum(data.filter(function(eventGroup) {
         return eventGroup.display === true;
     }));
+
     timeline(element);
-  });
 }
+
+function addStories(mediaOutlets) {
+    console.table(mediaOutlets);
+ 
+    table = '';
+    mediaOutlets.forEach(pub => {
+        table = table + '<tr><td colspan="2"><b>' + pub.name + '</b></td><td></td></tr>';
+
+        pub.data.forEach(story => {
+            table = table + '<tr><td>' + dateToYMD(new Date(story.date)) + '</td> <td>' + story.details.headline + '<td></tr>';
+        });
+    });
+
+    table = '<table style="width:90%">' + table + '</table>'; 
+
+    d3.select('#stories').html(table);
+}
+
+function popup(el) {
+/*     var table = '<table class="table table-striped table-bordered">';
+    if(el.hasOwnProperty("events")) {
+        table = table + '<thead>This is a group of ' + el.events.length + ' events starting on '+ el.date + '</thead><tbody>';
+        table = table + '<tr><th>Date</th><th>Event</th><th>Object</th></tr>';
+        for (var i = 0; i < el.events.length; i++) {
+            table = table + '<tr><td>' + el.events[i].date + ' </td> ';
+            for (var j in el.events[i].details) {
+                table = table +'<td> ' + el.events[i].details[j] + ' </td> ';
+            }
+            table = table + '</tr>';
+        }
+        table = table + '</tbody>';
+        } else {
+            table = table + 'Date: ' + el.date + '<br>';
+        for (i in el.details) {
+            table = table + i.charAt(0).toUpperCase() + i.slice(1) + ': ' + el.details[i] + '<br>';
+        }
+      }
+
+    $('#legend').html(table); */
+} 
 
 function addEthicsDocuments(modal, data) {
     modal.selectAll("h4")
@@ -378,10 +395,10 @@ var RowChart = function (facts, attribute, width, maxItems, height) {
         .labelOffsetX(5)
         .on('filtered', showFilters)
         .label(function (d) {
-            return d.key + " " + d.value;
+            return d.key; // + " " + d.value;
         });
 
-    //    .Axis().ticks(4).tickFormat(d3.format(".2s"));
+    //  .Axis().ticks(4).tickFormat(d3.format(".2s"));
 
     return chart;
 }
