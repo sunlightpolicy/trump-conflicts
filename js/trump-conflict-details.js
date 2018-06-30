661b00cfa4a6a9dfa64f689876ba11b0cb515232
smckissock <scottmckissock@gmail.com>
2018-06-30 05:20:19 -0400

    Removed unused divs

--- js/trump-conflict-details.js
+++ js/trump-conflict-details.js
@@ -1,16 +1,26 @@
 
 function timelinePopup(conflictId) {
+    var ethicsData = d3.json("data/ethics/" + conflictId + ".json", data => addHeader(data)); 
 
-    d3.json("data/ethics/" + conflictId + ".json", function(data) {
-        makeTimelinePopup(data, conflictId);
-    });
+    makeTimelinePopup(conflictId);
+}
+
+function addHeader(data) {
+    var conflict = d3.select('#conflict');
+    conflict.text(data.conflict);
+
+    var conflictDescription = d3.select('#conflictDescription');
+    conflictDescription.text(data.conflictDescription);
+
+    addEthics(data);
+    return data;
 }
 
 
-function makeTimelinePopup(ethicsData, conflictId) {
+function makeTimelinePopup(conflictId) {
+    //d3.select('#timeline').selectAll("svg").remove();
 
     var timeline = new TL.Timeline('timeline', 'data/timeline/' + conflictId + '.json', {
-        ga_property_id: "UA-27829802-4",
         is_embed:true
     });
     timeline._el.container.offsetHeight = 400;
@@ -23,41 +33,8 @@ function makeTimelinePopup(ethicsData, conflictId) {
         if (event.target == modal)
             modal.style.display = "none";
     }
-    //console.log(data);
-
     let modal = document.getElementById('ethicsModal');
     modal.style.display = "block";
-
-    var conflict = d3.select('#conflict');
-    //conflict.text(conflictData.name);
-
-    var conflictDescription = d3.select('#conflictDescription');
-    //conflictDescription.text(conflictData.description);
-
-    
-    d3.select('#timeline').selectAll("svg").remove();
-  
-
-    //addStories(mediaOutlets);
-    addEthics(ethicsData);
-}
-
-function addStories(mediaOutlets) {
-    console.table(mediaOutlets);
- 
-    var table = '';
-    mediaOutlets.forEach(pub => {
-        pub.data.forEach(story => {
-            table = 
-                table + 
-                '<tr>' +
-                    '<td class="conflict-col-1">' + dateToYMD(new Date(story.date)) + '</td>' +
-                    '<td class="conflict-col-2"><a href="' + story.details.link + '" target="_blank"><b>' + pub.name + '</b> / ' + story.details.headline + '</a><td>' + 
-                '</tr>';
-        });
-    });
-    table = '<table style="width:90%">' + table + '</table>'; 
-    d3.select('#stories').html('<h3>Media Accounts</h3>' + table);
 }
 
 
@@ -91,6 +68,7 @@ function addEthics(data) {
 }
 
 
+// Not used
 function addEthicsDocuments(modal, data) {
     
     modal.selectAll("h4")
@@ -112,27 +90,21 @@ function addEthicsDocuments(modal, data) {
         .append("hr");
 }
 
-
-// For Timeline
-function popup(el) {
-    /*     var table = '<table class="table table-striped table-bordered">';
-        if(el.hasOwnProperty("events")) {
-            table = table + '<thead>This is a group of ' + el.events.length + ' events starting on '+ el.date + '</thead><tbody>';
-            table = table + '<tr><th>Date</th><th>Event</th><th>Object</th></tr>';
-            for (var i = 0; i < el.events.length; i++) {
-                table = table + '<tr><td>' + el.events[i].date + ' </td> ';
-                for (var j in el.events[i].details) {
-                    table = table +'<td> ' + el.events[i].details[j] + ' </td> ';
-                }
-                table = table + '</tr>';
-            }
-            table = table + '</tbody>';
-            } else {
-                table = table + 'Date: ' + el.date + '<br>';
-            for (i in el.details) {
-                table = table + i.charAt(0).toUpperCase() + i.slice(1) + ': ' + el.details[i] + '<br>';
-            }
-          }
-    
-        $('#legend').html(table); */
-    } 
+// Not used - story links are in Timeline now
+function addStories(mediaOutlets) {
+    console.table(mediaOutlets);
+ 
+    var table = '';
+    mediaOutlets.forEach(pub => {
+        pub.data.forEach(story => {
+            table = 
+                table + 
+                '<tr>' +
+                    '<td class="conflict-col-1">' + dateToYMD(new Date(story.date)) + '</td>' +
+                    '<td class="conflict-col-2"><a href="' + story.details.link + '" target="_blank"><b>' + pub.name + '</b> / ' + story.details.headline + '</a><td>' + 
+                '</tr>';
+        });
+    });
+    table = '<table style="width:90%">' + table + '</table>'; 
+    d3.select('#stories').html('<h3>Media Accounts</h3>' + table);
+}
